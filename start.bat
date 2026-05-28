1859fc655bc93d37d276318cd8831ce4de2dc3db
Jeff Molofee <nehe@telus.net>
2026-05-28 03:34:31 -0600

    Upgrade pip before installing pygame to fix wheel build errors

--- start.bat
+++ start.bat
@@ -1,11 +1,20 @@
 @echo off
+echo Updating pip...
+python -m pip install --upgrade pip
+if errorlevel 1 (
+    echo.
+    echo ERROR: Could not update pip. Make sure Python 3.11+ is installed and in your PATH.
+    echo Download Python from https://www.python.org/downloads/
+    echo Make sure to check "Add Python to PATH" during installation.
+    pause
+    exit /b 1
+)
+echo.
 echo Installing dependencies...
 pip install pygame
 if errorlevel 1 (
     echo.
-    echo ERROR: pip failed. Make sure Python 3.11+ is installed and in your PATH.
-    echo Download Python from https://www.python.org/downloads/
-    echo Make sure to check "Add Python to PATH" during installation.
+    echo ERROR: Failed to install pygame. See above for details.
     pause
     exit /b 1
 )
